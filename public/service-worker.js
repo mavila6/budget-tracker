8f731849636cd5e61efe1c468d6d69ccc6fd36b0
Marco Avila <mavila6@email.arizona.com>
2021-10-04 15:55:05 -0700

    added the icons to the manifest and began the service worker js

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "bt-cache";
+const DATA_CACHE_NAME = "bt-data-cache";
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/js/idb.js',
+    '/css/styles.css',
+    '/js/index.js',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
+];
