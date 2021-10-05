d0eb68a57f353f6bb094f36540aa856f0a41e432
Marco Avila <mavila6@email.arizona.com>
2021-10-04 18:01:48 -0700

    added functions to idb file to save transactions when connection is not established and then upload them when back online

--- public/js/idb.js
+++ public/js/idb.js
@@ -19,3 +19,46 @@ request.onsuccess = e => {
 request.onerror = e => {
     console.log(e.target.errorCode);
 };
+// function to record & add transactions to your storewhen there is no internet connection
+function saveRecord(record) {
+    const transaction = db.transaction(["pending_transaction"], "readwrite");
+    const storedTransactions = transaction.objectStore("pending_transaction");
+    storedTransactions.add(record);
+};
+// function to upload all the saved transactions
+function updateTransactions() {
+    // variabes to access the database and get the records from the store 
+    const transaction = db.transaction(["pending_transaction"], "readwrite")
+    const storedTransactions = transaction.objectStore("pending_transaction")
+    const getAll = storedTransactions.getAll()
+    // runs function if getAll method is successfull
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json" 
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse)
+                }
+                const transaction = db.transaction(["pending_transaction"], "readWrite")
+                const storedTransactions = transaction.objectStore("pending_transaction")
+                storedTransactions.clear()
+                alert("All saved transactions have been submitted")
+            })
+            .catch(err => {
+                console.log(err)
+                response.status(400).json(err)
+            });
+        };
+    };
+};
+
+// Add event listener for when the user re-establishes a connection
+window.addEventListener("online", updateTransactions)
